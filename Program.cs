c7881d45c0459274f550bbef9f162138a377ccae
Dulce Alvarado <candyalvarado998@yahoo.com>
2022-11-14 19:20:54 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace LetsRun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LetsRun!!");
+            Console.WriteLine("\tDid you run Today? Y/N");
+            var responseDidTheyRun = Console.ReadKey().KeyChar;
+            var run = new Run;
+
+            if(responseDidTheyRun == 'y')
+            {
+                run.Ran = true;
+                Console.Writeline("Thanks for running!");
+            }
+            else if(responseDidTheyRun == 'n')
+            {
+                run.Ran = false;
+                Console.Writeline =("You should run more.")
+            }
+            else
+            {
+                Console.WriteLine("You entered an invalid option, logging error and quitting");
+                //Log error to txt file
+                return;
+            }
+
+            if(run.Ran == false) { return; }
+
+            Console.WriteLine("Since you ran, how far did you run? Enter Distance in Miles");
+            var responseHowFarRan = Console.ReadLine();
+            if(decimal.TryParse(responseHowFarRan, out decimal howFarRan))
+            {
+                Console.WriteLine("You entered an invalid option, logging error and quitting");
+                return;
+            }
+            run.Distance = howFarRan;
+
+            Console.Writeline("How long did it take you to run?");
+            if(TimeSpan.TryParse(Console.Readline(), out TimeSpan HowLongRan))
+            {
+                Console.Writeline("You entered an invalid option, logging error and quitting");
+                return;
+            }
+            run.Duration = howLongRan;
+
+            Console.WriteLine("Ran: = + run.Ran);
+            Console.WriteLine("Distance: " + run.Distance);
+            Console.Writeline("Duration: " + run.Duration);
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,6 @@
+﻿# Running App
+
+This project is an application for runners that will record the distance and duration of miles ran to track their progress.
+The runner will need to input the miles and length of time to get reports. The features inluded in this application will have
+a list with several values with at least one value retrieved, read data from a JSON file that deserializes to list,
+and a log that records errors back into JSON file.
